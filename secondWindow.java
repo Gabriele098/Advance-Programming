a7d055968395c1002037908100093c7110ff4c17
Gabriele Qazolli <58176354+Gabriele098@users.noreply.github.com>
2021-03-06 16:52:55 +0000

    Add files via upload

---
+++ secondWindow.java
@@ -0,0 +1,257 @@
+package JavaCW;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+
+public class secondWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+	private JTextField TextArea;
+	private JList OnlineClientsList;
+	private JTextArea textDisplay;
+	private JButton btnLogout;
+	private JRadioButton rdbtnPrivate_msg;
+	private JRadioButton rdbtnBroadcast_msg;
+
+	DataInputStream inputStream;
+	DataOutputStream outStream;
+	DefaultListModel<String> dm;
+	String id, clientIds = "";
+
+	/**
+	 * Launch the application.
+	 */
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					secondWindow window = new secondWindow();
+//					window.frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
+
+	/**
+	 * Create the application.
+	 */
+	public secondWindow() {
+		initialize();
+	}
+	
+	public secondWindow(String id, Socket s) { // constructor call, it will initialize required variables
+		initialize(); // Initialise UI components
+		this.id = id;
+		try {
+			frame.setTitle("Client View - " + id); // set title of UI
+			dm = new DefaultListModel<String>(); // default list used for showing active users on UI
+			OnlineClientsList.setModel(dm);// show that list on UI component JList named OnlineClientsList
+			inputStream = new DataInputStream(s.getInputStream()); // initilize input and output stream
+			outStream = new DataOutputStream(s.getOutputStream());
+			new Read().start(); // create a new thread for reading the messages
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	class Read extends Thread {
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					String m = inputStream.readUTF();  // read message from server, this will contain :;.,/=<comma seperated clientsIds>
+					System.out.println("inside read thread : " + m); // print message for testing purpose
+					if (m.contains(":;.,/=")) { // prefix(i know its random)
+						m = m.substring(6); // comma separated all active user ids
+						dm.clear(); // clear the list before inserting fresh elements
+						StringTokenizer st = new StringTokenizer(m, ","); // split all the clientIds and add to dm below
+						while (st.hasMoreTokens()) {
+							String u = st.nextToken();
+							if (!id.equals(u)) // we do not need to show own user id in the active user list pane
+								dm.addElement(u); // add all the active user ids to the defaultList to display on active
+													// user pane on client view
+						}
+					} else {
+						textDisplay.append("" + m + "\n"); //otherwise print on the clients message board
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					break;
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setForeground(Color.WHITE);
+		frame.getContentPane().setFont(new Font("Verdana Pro", Font.BOLD, 13));
+		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		frame.setBounds(100, 100, 713, 467);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textDisplay = new JTextArea();
+		textDisplay.setEditable(false);
+		textDisplay.setFont(new Font("Verdana Pro", Font.PLAIN, 13));
+		textDisplay.setBounds(20, 57, 404, 298);
+		frame.getContentPane().add(textDisplay);
+		
+		TextArea = new JTextField();
+		TextArea.setFont(new Font("Verdana Pro", Font.PLAIN, 13));
+		TextArea.setBounds(20, 365, 330, 30);
+		frame.getContentPane().add(TextArea);
+		
+		JButton btnSendButton = new JButton("SEND");
+		btnSendButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String textAreaMessage = TextArea.getText(); // get the message from textbox
+				if (textAreaMessage != null && !textAreaMessage.isEmpty()) {  // only if message is not empty then send it further otherwise do nothing
+					try {
+						String messageToBeSentToServer = "";
+						String cast = "broadcast"; // this will be an identifier to identify type of message
+						int flag = 0; // flag used to check whether used has selected any client or not for multicast 
+						if (rdbtnPrivate_msg.isSelected()) { // if 1-to-N is selected then do this
+							cast = "multicast"; 
+							List<String> clientList = OnlineClientsList.getSelectedValuesList(); // get all the users selected on UI
+							if (clientList.size() == 0) // if no user is selected then set the flag for further use
+								flag = 1;
+							for (String selectedUsr : clientList) { // append all the usernames selected in a variable
+								if (clientIds.isEmpty())
+									clientIds += selectedUsr;
+								else
+									clientIds += "," + selectedUsr;
+							}
+							messageToBeSentToServer = cast + ":" + clientIds + ":" + textAreaMessage; // prepare message to be sent to server
+						} else {
+							messageToBeSentToServer = cast + ":" + textAreaMessage; // in case of broadcast we don't need to know userIds
+						}
+						if (cast.equalsIgnoreCase("multicast")) { 
+							if (flag == 1) { // for multicast check if no user was selected then prompt a message dialog
+								JOptionPane.showMessageDialog(frame, "No user selected");
+							} else { // otherwise just send the message to the user
+								outStream.writeUTF(messageToBeSentToServer);
+								TextArea.setText("");
+								textDisplay.append("< You sent msg to " + clientIds + ">" + textAreaMessage + "\n"); //show the sent message to the sender's message board
+							}
+						} else { // in case of broadcast
+							outStream.writeUTF(messageToBeSentToServer);
+							TextArea.setText("");
+							textDisplay.append("< You sent msg to All >" + textAreaMessage + "\n");
+						}
+						clientIds = ""; // clear the all the client ids 
+					} catch (Exception ex) {
+						JOptionPane.showMessageDialog(frame, "User does not exist anymore."); // if user doesn't exist then show message
+					}
+				}
+			
+			}
+		});
+		btnSendButton.setBackground(Color.LIGHT_GRAY);
+		btnSendButton.setFont(new Font("Verdana Pro", Font.BOLD, 11));
+		btnSendButton.setBounds(352, 365, 72, 30);
+		frame.getContentPane().add(btnSendButton);
+		
+		JLabel lblOnlineClientsLabel = new JLabel("Online Clients");
+		lblOnlineClientsLabel.setForeground(Color.WHITE);
+		lblOnlineClientsLabel.setFont(new Font("Verdana Pro", Font.BOLD, 13));
+		lblOnlineClientsLabel.setBounds(478, 80, 130, 14);
+		frame.getContentPane().add(lblOnlineClientsLabel);
+		
+		JLabel lblClientName = new JLabel("#name of the client youre talking with");
+		lblClientName.setForeground(Color.WHITE);
+		lblClientName.setFont(new Font("Verdana Pro", Font.BOLD, 13));
+		lblClientName.setBounds(20, 28, 314, 24);
+		frame.getContentPane().add(lblClientName);
+		
+		OnlineClientsList = new JList();
+		OnlineClientsList.setFont(new Font("Verdana Pro", Font.PLAIN, 13));
+		OnlineClientsList.setBounds(479, 105, 197, 248);
+		frame.getContentPane().add(OnlineClientsList);
+		
+		btnLogout = new JButton("LOGOUT");
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					outStream.writeUTF("exit"); // closes the thread and show the message on server and client's message
+												// board
+					textDisplay.append("You are disconnected now.\n");
+					frame.dispose(); // close the frame 
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnLogout.setFont(new Font("Verdana Pro", Font.BOLD, 11));
+		btnLogout.setBackground(Color.LIGHT_GRAY);
+		btnLogout.setBounds(584, 361, 92, 30);
+		frame.getContentPane().add(btnLogout);
+		
+		rdbtnPrivate_msg = new JRadioButton("Private");
+		rdbtnPrivate_msg.setSelected(true);
+		rdbtnPrivate_msg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				OnlineClientsList.setEnabled(true);
+			}
+		});
+		rdbtnPrivate_msg.setForeground(Color.WHITE);
+		rdbtnPrivate_msg.setBackground(Color.DARK_GRAY);
+		rdbtnPrivate_msg.setFont(new Font("Verdana Pro", Font.BOLD, 11));
+		rdbtnPrivate_msg.setBounds(479, 43, 92, 23);
+		frame.getContentPane().add(rdbtnPrivate_msg);
+		
+		rdbtnBroadcast_msg = new JRadioButton("Broadcast");
+		rdbtnBroadcast_msg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				OnlineClientsList.setEnabled(false);
+			}
+		});
+		rdbtnBroadcast_msg.setForeground(Color.WHITE);
+		rdbtnBroadcast_msg.setFont(new Font("Verdana Pro", Font.BOLD, 11));
+		rdbtnBroadcast_msg.setBackground(Color.DARK_GRAY);
+		rdbtnBroadcast_msg.setBounds(573, 43, 111, 23);
+		frame.getContentPane().add(rdbtnBroadcast_msg);
+		
+		ButtonGroup btngrp = new ButtonGroup();
+		btngrp.add(rdbtnPrivate_msg);
+		btngrp.add(rdbtnBroadcast_msg);
+
+		frame.setVisible(true);
+		
+		JButton btnCloseChat = new JButton("CLOSE");
+		btnCloseChat.setFont(new Font("Verdana Pro", Font.BOLD, 11));
+		btnCloseChat.setBackground(Color.LIGHT_GRAY);
+		btnCloseChat.setBounds(344, 26, 80, 30);
+		frame.getContentPane().add(btnCloseChat);
+	}
+
+
+}
+
---
+++ serverWindow.java
@@ -0,0 +1,257 @@
+package JavaCW;
+
+import java.awt.EventQueue;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.swing.JTextArea;
+import javax.swing.JList;
+import javax.swing.JLabel;
+
+public class serverWindow {
+
+	private JFrame frmServer;
+	private ServerSocket serverSocket; //server socket variable
+	private static int port = 4999;  // port number to be used
+	private JTextArea txtrDisplayServer; // variable for server message board on UI
+	private JList AllClientsServerList;  // variable on UI
+	private JList OnlineClientsServerList; // variable on UI
+	private static final long serialVersionUID = 1L;
+	private static Map<String, Socket> allUsersList = new ConcurrentHashMap<>(); // keeps the mapping of all the																				// usernames used and their socket connections
+	private static Set<String> activeUserSet = new HashSet<>(); // this set keeps track of all the active users 
+	private DefaultListModel<String> activeDlm = new DefaultListModel<String>(); // keeps list of active users for display on UI
+	private DefaultListModel<String> allDlm = new DefaultListModel<String>(); // keeps list of all users for display on UI
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					serverWindow window = new serverWindow();
+					window.frmServer.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public serverWindow() {
+		initialize();  // components of swing app will be initialized here.
+		try {
+			serverSocket = new ServerSocket(port);  // create a socket for server
+			txtrDisplayServer.append("Server is running...\n");
+			txtrDisplayServer.append("Server Port Number: " + port + "\n"); // print messages to server message board
+			txtrDisplayServer.append("Waiting for the clients...\n");
+			new ClientAccept().start(); // this will create a thread for client
+		} catch (Exception e) {
+			System.out.println("errori server nel serverview");
+			e.printStackTrace();
+		}
+	}
+	
+	class ClientAccept extends Thread {
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					Socket clientSocket = serverSocket.accept();  // create a socket for client
+					String uName = new DataInputStream(clientSocket.getInputStream()).readUTF(); // this will receive the username sent from client register view
+					DataOutputStream cOutStream = new DataOutputStream(clientSocket.getOutputStream()); // create an output stream for client
+					if (activeUserSet != null && activeUserSet.contains(uName)) { // if username is in use then we need to prompt user to enter new name
+						cOutStream.writeUTF("ID already connected!");
+					} else {
+						allUsersList.put(uName, clientSocket); // add new user to allUserList and activeUserSet
+						activeUserSet.add(uName);
+						cOutStream.writeUTF(""); // clear the existing message
+						activeDlm.addElement(uName); // add this user to the active user JList
+						if (!allDlm.contains(uName)) // if username taken previously then don't add to allUser JList otherwise add it
+							allDlm.addElement(uName);
+						OnlineClientsServerList.setModel(activeDlm); // show the active and allUser List to the swing app in JList
+						AllClientsServerList.setModel(allDlm);
+						txtrDisplayServer.append("Client " + uName + " Connected...\n"); // print message on server that new client has been connected.
+						new MsgRead(clientSocket, uName).start(); // create a thread to read messages
+						new PrepareCLientList().start(); //create a thread to update all the active clients
+					}
+				} catch (IOException ioex) {  // throw any exception occurs
+					System.out.println("errori server nel client accept1");
+					ioex.printStackTrace();
+				} catch (Exception e) {
+					System.out.println("errori server nel client accept2");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	class MsgRead extends Thread { // this class reads the messages coming from client and take appropriate actions
+		Socket s;
+		String Id;
+		private MsgRead(Socket s, String uname) { // socket and username will be provided by client
+			this.s = s;
+			this.Id = uname;
+		}
+
+		@Override
+		public void run() {
+			while (AllClientsServerList != null && !allUsersList.isEmpty()) {  // if allUserList is not empty then proceed further
+				try {
+					String message = new DataInputStream(s.getInputStream()).readUTF(); // read message from client
+					System.out.println("message read ==> " + message); // just print the message for testing
+					String[] msgList = message.split(":"); // I have used my own identifier to identify what action to take on the received message from client
+														// i have appended actionToBeTaken:clients_for_receiving_msg:message
+					if (msgList[0].equalsIgnoreCase("multicast")) { // if action is multicast then send messages to selected active users
+						String[] sendToList = msgList[1].split(","); //this variable contains list of clients which will receive message
+						for (String usr : sendToList) { // for every user send message
+							try {
+								if (activeUserSet.contains(usr)) { // check again if user is active then send the message
+									new DataOutputStream(((Socket) allUsersList.get(usr)).getOutputStream())
+											.writeUTF("< " + Id + " >" + msgList[2]); // put message in output stream
+								}
+							} catch (Exception e) { // throw exceptions
+								e.printStackTrace();
+							}
+						}
+					} else if (msgList[0].equalsIgnoreCase("broadcast")) { // if broadcast then send message to all active clients
+						
+						Iterator<String> itr1 = allUsersList.keySet().iterator(); // iterate over all users
+						while (itr1.hasNext()) {
+							String usrName = (String) itr1.next(); // it is the username
+							if (!usrName.equalsIgnoreCase(Id)) { // we don't need to send message to ourself, so we check for our Id
+								try {
+									if (activeUserSet.contains(usrName)) { // if client is active then send message through output stream
+										new DataOutputStream(((Socket) allUsersList.get(usrName)).getOutputStream())
+												.writeUTF("< " + Id + " >" + msgList[1]);
+									} else {
+										//if user is not active then notify the sender about the disconnected client
+										new DataOutputStream(s.getOutputStream())
+												.writeUTF("Message couldn't be delivered to user " + usrName + " because it is disconnected.\n");
+									}
+								} catch (Exception e) {
+									System.out.println("errori server nel run");
+									e.printStackTrace(); // throw exceptions
+								}
+							}
+						}
+					} else if (msgList[0].equalsIgnoreCase("exit")) { // if a client's process is killed then notify other clients
+						activeUserSet.remove(Id); // remove that client from active usre set
+						txtrDisplayServer.append(Id + " disconnected....\n"); // print message on server message board
+
+						new PrepareCLientList().start(); // update the active and all user list on UI
+
+						Iterator<String> itr = activeUserSet.iterator(); // iterate over other active users
+						while (itr.hasNext()) {
+							String usrName2 = (String) itr.next();
+							if (!usrName2.equalsIgnoreCase(Id)) { // we don't need to send this message to ourself
+								try {
+									new DataOutputStream(((Socket) allUsersList.get(usrName2)).getOutputStream())
+											.writeUTF(Id + " disconnected..."); // notify all other active user for disconnection of a user
+								} catch (Exception e) { // throw errors
+									System.out.println("errori server nel run2");
+									e.printStackTrace();
+								}
+								new PrepareCLientList().start(); // update the active user list for every client after a user is disconnected
+							}
+						}
+						activeDlm.removeElement(Id); // remove client from Jlist for server
+						OnlineClientsServerList.setModel(activeDlm); //update the active user list
+					}
+				} catch (Exception e) {
+					System.out.println("errori server nel run3");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	class PrepareCLientList extends Thread { // it prepares the list of active user to be displayed on the UI
+		@Override
+		public void run() {
+			try {
+				String ids = "";
+				Iterator itr = activeUserSet.iterator(); // iterate over all active users
+				while (itr.hasNext()) { // prepare string of all the users
+					String key = (String) itr.next();
+					ids += key + ",";
+				}
+				if (ids.length() != 0) { // just trimming the list for the safe side.
+					ids = ids.substring(0, ids.length() - 1);
+				}
+				itr = activeUserSet.iterator(); 
+				while (itr.hasNext()) { // iterate over all active users
+					String key = (String) itr.next();
+					try {
+						new DataOutputStream(((Socket) allUsersList.get(key)).getOutputStream())
+								.writeUTF(":;.,/=" + ids); // set output stream and send the list of active users with identifier prefix :;.,/=
+					} catch (Exception e) {
+						System.out.println("errori server nel prepare1");
+						e.printStackTrace();
+					}
+				}
+			} catch (Exception e) {
+				System.out.println("errori server nel prepare2");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmServer = new JFrame();
+		frmServer.setTitle("Server");
+		frmServer.getContentPane().setForeground(Color.WHITE);
+		frmServer.getContentPane().setFont(new Font("Verdana Pro", Font.BOLD, 13));
+		frmServer.getContentPane().setBackground(Color.DARK_GRAY);
+		frmServer.setBounds(100, 100, 713, 467);
+		frmServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmServer.getContentPane().setLayout(null);
+		
+		txtrDisplayServer = new JTextArea(); 
+		txtrDisplayServer.setEditable(false);
+		txtrDisplayServer.setFont(new Font("Verdana Pro", Font.PLAIN, 13));
+		txtrDisplayServer.setBackground(new Color(255, 255, 255));
+		txtrDisplayServer.setBounds(20, 43, 404, 346);
+		frmServer.getContentPane().add(txtrDisplayServer);
+		//txtrDisplayServer.setText("Server is running...\r\n");
+		
+		OnlineClientsServerList = new JList();
+		OnlineClientsServerList.setBounds(481, 66, 197, 135);
+		frmServer.getContentPane().add(OnlineClientsServerList);
+		
+		AllClientsServerList = new JList();
+		AllClientsServerList.setBounds(481, 254, 197, 135);
+		frmServer.getContentPane().add(AllClientsServerList);
+		
+		JLabel lblOnlineClientsServerLabel = new JLabel("Online Clients");
+		lblOnlineClientsServerLabel.setForeground(Color.WHITE);
+		lblOnlineClientsServerLabel.setFont(new Font("Verdana Pro", Font.BOLD, 13));
+		lblOnlineClientsServerLabel.setBounds(481, 43, 130, 14);
+		frmServer.getContentPane().add(lblOnlineClientsServerLabel);
+		
+		JLabel lblAllClientsServerLabel = new JLabel("All Clients");
+		lblAllClientsServerLabel.setForeground(Color.WHITE);
+		lblAllClientsServerLabel.setFont(new Font("Verdana Pro", Font.BOLD, 13));
+		lblAllClientsServerLabel.setBounds(481, 232, 130, 14);
+		frmServer.getContentPane().add(lblAllClientsServerLabel);
+	}
+}
